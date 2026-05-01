8a70aab57ed4961a16bdbc8ca380b5cf982084a3
Siraken-05xl <siro04xl@gmail.com>
2026-05-01 17:52:38 +0200

    Main Menu (acabado)

---
+++ FPS_Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SCN_Level");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
